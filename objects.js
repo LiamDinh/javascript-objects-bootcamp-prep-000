4385f66e7f7552fc7878b1a352e9ed8b88d3b357
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-08 23:06:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(obj, artistName, songTitle) {
   obj[artistName] = songTitle
   return obj 
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist 
+}
